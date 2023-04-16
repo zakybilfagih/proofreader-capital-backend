93f08164b76a890f80beb70a79f49303371be3e4
SultanDaffaN <nusantarasulltan@gmail.com>
2023-04-17 05:53:54 +0700

    remove detokenize

--- detokenize.py
+++
@@ -1,7 +0,0 @@
-from sacremoses import MosesDetokenizer
-
-detokenizer = MosesDetokenizer()
-
-def detokenize_sentence(lst):
-  detokenized_sentence = detokenizer.detokenize(lst)
-  return detokenized_sentence
